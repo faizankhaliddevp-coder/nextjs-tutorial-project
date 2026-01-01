15e941d841e966a9d7e76eabfffb4c1df917a130
faizankhaliddevp-coder <faizan.khalid.devp@gmail.com>
2026-01-01 13:40:08 +0500

    push code

---
+++ nextjs-dashboard/app/dashboard/layout.tsx
@@ -0,0 +1,15 @@
+import SideNav from '@/app/ui/dashboard/sidenav';
+ 
+export default function Layout({ children }: { children: React.ReactNode }) {
+  return (
+    <div className="flex h-screen flex-col md:flex-row md:overflow-hidden">
+      <div className="w-full flex-none md:w-64">
+        <SideNav />
+      </div>
+      <div className="grow p-6 md:overflow-y-auto md:p-12">{children}</div>
+    </div>
+  );
+}
+
+// The <Layout /> component receives a children prop. This child can either be a page or another layout.
+//  In your case, the pages inside /dashboard will automatically be nested inside a <Layout /> like so:
